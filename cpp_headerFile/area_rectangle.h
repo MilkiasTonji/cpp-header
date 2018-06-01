a5e8cdafa23cf53ef1a25b4ee46e58a299aa40e8
MilkiasTonji <37698254+MilkiasTonji@users.noreply.github.com>
2018-06-01 05:27:13 -0700

    Add files via upload

---
+++ cpp_headerFile/area_rectangle.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+
+double areaOfRectangle(double width, double height){
+    double area_rectangle = width * height;  
+
+}
+double perimeter(double width, double height){
+	double perimeter_rectangle = 2 * (width * height);
+}
---
+++ cpp_headerFile/mainArea.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+	double width;
+	double height;
+	cout<<"Enter width: ";
+	cin>>width;
+	
+	cout<<"Enter height: ";
+	cin>>height;
+	cout<<"The area of the rectangle is: ";
+	cout<<areaOfRectangle(width, height)<< end1;
+	cout<<"and the perimeter of the rectangle is: ";
+	cout<<perimeter(width, height) << end1;
+	
+	return 0;
+}
